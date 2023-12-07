7e9e3552b582449356d596a8d45723603215ae00
RomainVReco <145270898+RomainVReco@users.noreply.github.com>
2023-12-07 10:59:31 +0100

    Player interaction (#19)
    * Maj
    
    * Maj2
    
    ---------
    
    Co-authored-by: WillyB89 <walid.ben-aissa@edu.esiee-it.fr>

--- src/Room/Room.java
+++ src/Room/Room.java
@@ -1,6 +1,8 @@
 package Room;
 import Character.*;
+import GestionUtilisateur.GestionUser;
 import Item.*;
+import Weapon.Weapon;
 
 import java.util.HashMap;
 
@@ -9,6 +11,8 @@ public class Room implements Searchable {
     MonsterFactory monsterFactory;
     private HashMap<String, Item> setOfItems = new HashMap<>();
     private boolean hasBeenSearched = false;
+
+    GestionUser gestionUser = new GestionUser();
 
     public Room(int p_iIndex) {
         this.monsterFactory = new MonsterFactory();
@@ -28,18 +32,52 @@ public class Room implements Searchable {
      * @param hero : le héros créé à l'initialisation du jeu
      */
     public void enterRoom(Hero hero) {
+        // Notifier l'hero de la presence d'un monstre dans la piece
         hero.discoverEnnemy(this.monster);
+
+        // Continuer le combat jusqu'à ce que le monstre ou l'hero soit mort
         while (this.monster.isAlive() && hero.isAlive()) {
-            this.monster.attack(hero);
+            // le monstre attaque l'hero
+              this.monster.attack(hero);
+
+            // verifier si l'hero est encore en vie avant de lui permettre de contre-attaquer
             if (hero.isAlive()) {
+                System.out.println();
+                String weaponChoice = gestionUser.promptString("Which weapon would you like to use to attack the monster?");
+
+            if (weaponChoice)
+                // L'hero counter-attaque le monstre
                 hero.attack(this.monster);
-            } else return;
+               // System.out.println("Hero responds to the attack with its " + hero.getWeapon() +
+                        //" and inflicts " + heroAttackDamage + " damage.");
+                //System.out.println(this.monster.getMonsterName() + " has been hit and has now " + this.monster.getHealth() + " life points left");
+            } else {
+
+                return;
+            }
         }
-        // implanter une demande pour fouiller la pièce et/ou le monstre
-        hero.searchForPotions(this);
-        hero.searchForPotions(this.monster);
-        System.out.println("The room has been purged");
+
+        // Apres le combat, demander au Hero s'il veut fouiller la piece
+        char searchRoom = gestionUser.promptYesNo("Do you want to search the room? (Y/N)");
+
+        if (searchRoom == 'Y' || searchRoom == 'y') {
+            // L'Hero chercher des potions dans la piece
+            hero.searchForPotions(this);
+        }
+
+        // Demander au Hero s'il veut fouiller le monstre qu'il a tué
+        char searchMonster = gestionUser.promptYesNo("Do you want to search the defeated monster? (Y/N)");
+        if (searchMonster == 'Y' || searchMonster == 'y') {
+            hero.searchForPotions(this.monster);
+        }
+
+        System.out.println("La pièce a été purgée de ses éléments impurs");
     }
+
+    /**
+     * la méthode search va déclencher la recherche de potion
+     * @return
+     */
 
     @Override
     public Item search() {
