0c6e2130fb86cf621ea32043319984fc9a6a59ee
RomainV <romain.vorler@gmail.com>
2023-12-08 14:26:25 +0100

    augmentation dégat opinel

--- src/Weapon/Monster/Opinel.java
+++ src/Weapon/Monster/Opinel.java
@@ -5,6 +5,6 @@ public class Opinel extends MonsterWeapon {
     public Opinel(){
         this.setMaxRoundBeforeCritical(1);
         this.setWeaponName("Opinel");
-        this.setBonusDamage(3);
+        this.setBonusDamage(5);
     }
 }
